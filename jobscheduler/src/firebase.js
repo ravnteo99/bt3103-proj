0aa77297fadfe1a8a5cc5ef5cdfa5ebcdd7f73cc
glennbjorn <77476786+glennbjorn@users.noreply.github.com>
2022-10-11 15:53:18 +0800

    added firebase

---
+++ jobscheduler/src/firebase.js
@@ -0,0 +1,15 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBwT7mzK-SZdOHvvx2ECPqd7f_iLnHOej0",
+  authDomain: "bt3103-job-scheduler.firebaseapp.com",
+  projectId: "bt3103-job-scheduler",
+  storageBucket: "bt3103-job-scheduler.appspot.com",
+  messagingSenderId: "649357689559",
+  appId: "1:649357689559:web:c793e04be2bcc7ac15e692",
+  measurementId: "G-W9PLYBCNKC"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
