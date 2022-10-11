b0b63fc7dc9179456c26ff5ac38f59de601ce0aa
ravnteo99 <e0544178@u.nus.edu>
2022-10-11 18:25:58 +0800

    updated CSS of login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -35,10 +35,58 @@
 </script>
 
 <style scoped>
+h1 {
+    font-family: sans-serif;
+}
+p {
+    font-family: sans-serif;
+    font-size: 16px;
+}
 .register1 {
     margin-top: -10px;
 }
 .register2 {
     margin-top: -30px;
+    margin-bottom: 10px;
 }
+form {
+    font-family: sans-serif;
+}
+label {
+    font-size: 14px;
+    font-weight: bold;
+}
+input {
+    -webkit-appearance: none;
+    font-size: 14px;
+    height: 30px;
+    width: 280px;
+    box-sizing: border-box;
+    border-color: lightgray;
+    border-radius: 8px;
+    border-width: 1px;
+}
+input:focus {
+    border-color: black;
+}
+button {
+    -webkit-appearance: none;
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center;
+    height: 30px;
+    width: 280px;
+    background-color: #F8D57E;
+    border-radius: 8px;
+    border: none;
+    cursor: pointer;
+}
+button:hover{
+    background-color: #fcca4c;
+}
+button:active {
+    opacity: 0.6;
+    transform: translateY(3px);
+}
+
 </style>
