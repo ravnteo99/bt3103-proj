b0980025bbd6b9b4fd6412b8bf6d30f319b77e7c
ravnteo99 <e0544178@u.nus.edu>
2022-10-11 18:03:01 +0800

    edited CSS of login page

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,25 +1,33 @@
 <template>
-    <div id="wrapper">
+        <h1 class="signin">Sign In</h1>
+        <br>
+        <p class="register1">If you don't have an account registered,</p>
+        <br>
+        <p class="register2">you can register here!</p>
+        <br>
         <form id="login">
+            <label for="email">Email Address</label><br>
             <input 
                 type="email"
                 name="email"
                 id="email"
                 v-model="email"
-                placeholder="Email Address" required 
+                placeholder="someone@example.com" required 
             />
-            <br />
+            <br><br>
+            <label for="password">Password</label><br>
             <input 
                 type="password"
                 name="password"
                 id="password"
                 v-model="password"
-                placeholder="Password"
+                placeholder="Enter your password"
                 required
             />
-            <br />
+            <br><br>
+
+            <button type="submit">Sign In</button><br>
         </form>
-    </div>
 </template>
 
 <script>
@@ -27,4 +35,10 @@
 </script>
 
 <style scoped>
+.register1 {
+    margin-top: -10px;
+}
+.register2 {
+    margin-top: -30px;
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,21 @@
 <template>
-<LoginForm />
+<div id="wrapper">
+    <img :src="logo" />
+    <div id="login">
+        <LoginForm />
+    </div>
+</div>
 </template>
 
 <script>
 import LoginForm from '@/components/LoginForm.vue'
 export default {
     name: 'App',
+    data() {
+        return {
+            logo: require('@/assets/logo.png')
+        }
+    },
     components: {
         LoginForm
     }
@@ -13,4 +23,23 @@ export default {
 </script>
 
 <style scoped>
+#wrapper {
+    background-color: #F9EBC9;
+    height: 100vh;
+    width: 100vw;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+img {
+    width: 300px;
+    height: 300px;
+    float: left;
+    padding: 20px;
+    margin-right: 100px;
+    align-self: center;
+}
 </style>
