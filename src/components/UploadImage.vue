fdb97ad9be95054ae1331199e2f1c04ace69cf40
Hazel <e0544178@u.nus.edu>
2022-10-23 23:47:43 +0800

    updated css of uploadimage

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -1,7 +1,10 @@
 <template>
-   <div>
+   <div id="uploadImage">
       <img v-if="uploaded" :src="previewImage" class="profilePic" />
-      <input type="file" accept="image/jpeg" @change=uploadImage>
+      <label class="chooseFile">
+        <input type="file" accept="image/jpeg" @change=uploadImage>
+        Upload Image 
+      </label>
    </div>
 </template>
 
@@ -28,13 +31,35 @@
      }  
 </script>
 
-
-
 <style scoped>
+#uploadImage {
+    margin-top: 5px;
+}
 .profilePic{
     display:flex;
     width: 100px;
     height: 100px;
     border: none;
+    margin-bottom: 10px;
+}
+input[type="file"] {
+    display: none;
+}
+label {
+    font-size: 12px;
+    font-weight: bold;
+    text-align: center;
+    padding: 5px 100px;
+    background-color: #F8D57E;
+    border-radius: 8px;
+    border: none;
+    cursor: pointer;
+}
+label:hover{
+    background-color: #fcca4c;
+}
+label:active {
+    opacity: 0.6;
+    transform: translateY(3px);
 }
  </style>
