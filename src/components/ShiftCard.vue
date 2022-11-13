69fe63d0cc94fbbaadd01476385f5f9b5e2fec66
glennbjorn <77476786+glennbjorn@users.noreply.github.com>
2022-11-13 18:27:30 +0800

    css changes

--- src/components/ShiftCard.vue
+++ src/components/ShiftCard.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="shift-card-wrapper">
-    <h2>{{ branch }}</h2>
-    <p>{{ title }}</p>
+  <div :class="branchclass">
+    <div class="shift-card-wrapper">
+      <h2>{{ branch }}</h2>
+      <p>{{ title }}</p>
+      Date: {{ date }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["branch", "title"],
+  data() {
+    return {
+      branchclass: this.branch.replace(/\s/g, ""),
+    };
+  },
+  props: ["branch", "title", "date", "startTime", "endTime"],
 };
 </script>
 
@@ -15,10 +23,29 @@ export default {
 .shift-card-wrapper {
   display: flex;
   flex-direction: column;
-  background-color: #fbfbfd;
   border-radius: 10px;
   width: 300px;
   height: 150px;
-  line-height: 0px;
+  padding: 5px;
 }
+
+.shift-card-wrapper:hover {
+  cursor: pointer;
+}
+
+h2 {
+  margin-bottom: 0px;
+}
+
+.AngMoKioHub {
+  background-color: rgb(244, 182, 187);
+  border-radius: 10px;
+}
+
+.Junction8 {
+  background-color: rgb(250, 224, 191);
+  border-radius: 10px;
+}
+
+/* 255,255,193 201,253,205 194,224,252 */
 </style>
