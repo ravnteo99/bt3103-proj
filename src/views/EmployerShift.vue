ece789dff3b012c1bd5a0a3a51e6a48d3463781e
glennbjorn <77476786+glennbjorn@users.noreply.github.com>
2022-11-16 20:05:59 +0800

    remove branch filter in shift management

--- src/views/EmployerShift.vue
+++ src/views/EmployerShift.vue
@@ -6,12 +6,6 @@
 
     <h1 class="section-title">Shifts Information</h1>
 
-  <!--  branch filter for shifts-->
-    <select name="branch" id="branch" class="branch-filter input-field" v-model="selectedBranch">
-      <option value="" selected>All branches</option>
-      <option v-for="branch in branches" :value="branch" :key="branch.name"> {{ branch.name }} </option>
-    </select>
-
     <div class="date-indicator">
       <div class="date-input">
         <span>Viewing Shifts from: </span>
